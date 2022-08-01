2ad3e5acbbb09209d4c0f19a31a27b1b76f7d5cc
Bahadır Kalaçay <36983849+bahadirkalacay@users.noreply.github.com>
2022-08-01 10:09:01 +0300

    Update styles.js

--- client/src/components/Auth/styles.js
+++ client/src/components/Auth/styles.js
@@ -7,6 +7,7 @@ export default makeStyles ((theme) => ({
         flexDirection: 'column',
         alignItems: 'center',
         padding: theme.spacing(2),
+        width:"85%"
     },
 
     root: {
